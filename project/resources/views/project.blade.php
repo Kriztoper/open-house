2ca18d97b0f4eb8393e0e3a93dec7a9eb914cd77
Zyraphyl\Zyra <zlpearanda@up.edu.ph>
2016-09-28 17:10:11 +0800

    Corrected website name

--- project/resources/views/project.blade.php
+++ project/resources/views/project.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Student Scheduler</title>
+        <title>Open House 2016</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
