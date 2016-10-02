147d3b7568ffe1337ce42cece862a90a77acad11
James Babalcon <jrbabalcon@up.edu.ph>
2016-10-02 17:53:48 +0800

    dashboard changes

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -4,32 +4,25 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width = device-width, initial-scale = 1">
 	
-	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
-	
 	<title>KOMSAI OPENHAUS</title>
 
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-
-	<!-- Latest compiled and minified JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/jquery.slick/1.6.0/slick.css"/>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/dashboard.css">
 </head>
 <body>
 
 	<div class="container-fluid">
-		<nav class="navbar navbar-fixed-top">
+		<nav id="top-navbar" class="navbar navbar-default">
 			<div class="container-fluid">
     			<div class="navbar-header">
      				<a class="navbar-brand" href="#">
-     					<a href="http://imgur.com/js8s2AR"><img src="http://i.imgur.com/js8s2AR.png" title="source: imgur.com" style="margin-top: 10px; margin-right: 5px" /></a>
+     					<img src="http://i.imgur.com/js8s2AR.png" style="margin: auto !important; position: center" width=80%/>
      				</a>
   				</div>
-    			<ul class="nav navbar-nav">
+    			<ul id="tokens" class="nav navbar-nav">
 	      			<li>
-	      				<p class="navbar-text"><span class="glyphicon glyphicon-record white"></span><span class="red"> x </span><span class="white">999</span></p>
+	      				<p class="navbar-text"><span class="glyphicon glyphicon-record white"></span><span class="red"> x </span><span class="white">{{'69'}}</span></p>
 	      			</li>
 	    		</ul>
 	    		<ul id="links" class="nav navbar-nav navbar-right">
@@ -43,6 +36,14 @@
   			</div>
 		</nav>
 			
+		<div class="jumbotron">
+			<div class="container-fluid">
+				<h1 class="page-header">
+					WELCOME TO<br>KOMSAI OPENHOUSE
+				</h1>
+			</div>
+		</div>
+
 		<nav class="navbar navbar-fixed-bottom">
 			<div class="container-fluid">
 				<ul class="nav navbar-nav navbar-right">
@@ -51,5 +52,8 @@
 			</div>
 		</nav>
     </div>
+
+    <script type="text/javascript" src="http://code.jquery.com/jquery-3.1.1.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
