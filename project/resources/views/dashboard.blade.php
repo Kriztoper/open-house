ed53ad83a8fa891d3494e69beccc1ab830605532
James Babalcon <jrbabalcon@up.edu.ph>
2016-10-02 11:50:43 +0800

    dashboard changes

--- project/resources/views/dashboard.blade.php
+++ project/resources/views/dashboard.blade.php
@@ -3,17 +3,23 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width = device-width, initial-scale = 1">
+	
 	<link rel="stylesheet" type="text/css" href="css/dashboard.css">
+	
 	<title>KOMSAI OPENHAUS</title>
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </head>
 <body>
 
-	<div class="container">
+	<div class="container-fluid">
 		<nav class="navbar navbar-fixed-top">
 			<div class="container-fluid">
     			<div class="navbar-header">
@@ -26,21 +32,21 @@
 	      				<p class="navbar-text"><span class="glyphicon glyphicon-record white"></span><span class="red"> x </span><span class="white">999</span></p>
 	      			</li>
 	    		</ul>
-	    		<ul id="links" class="nav navbar-nav navbar-right gobold">
+	    		<ul id="links" class="nav navbar-nav navbar-right">
 	    			<li class="active"><a href="#">DASHBOARD</a></li>
 	      			<li><a href="#">HALL OF FAME</a></li>
 	      			<li><a href="#">GAMES</a></li>
 	      			<li><a href="#">VIDEOS</a></li>
 	      			<!-- Change to user name -->
-	      			<li><a href="#">JAMES</a></li>
+	      			<li><a href="#">{{'JAMES'}}</a></li>
 	    		</ul>
   			</div>
 		</nav>
-
+			
 		<nav class="navbar navbar-fixed-bottom">
 			<div class="container-fluid">
 				<ul class="nav navbar-nav navbar-right">
-					<li><a href="#">About the Developers</a></li>
+					<li><a href="#">ABOUT THE DEVELOPERS</a></li>
 				</ul>
 			</div>
 		</nav>
