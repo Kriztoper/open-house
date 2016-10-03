c2eab5c7c685eafdd80c02ead2f34e012b5ff600
ACER\Charlotte Sacmar <charlotte.sacmar@gmail.com>
2016-10-03 19:08:50 +0800

    fixed login and reg designs (reg still lacks addt'l gif for bg)

--- project/resources/views/auth/login.blade.php
+++ project/resources/views/auth/login.blade.php
@@ -37,50 +37,52 @@
           </div>
         </form>  -->
         @if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
+        <div class="alert alert-danger">
+          <strong>Whoops!</strong> There were some problems with your input.<br><br>
+          <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+            <li>{{ $error }}</li>
             @endforeach
-        </ul>
-    </div>
-@endif
+          </ul>
+        </div>
+        @endif
 
         <form class="form-horizontal" role="form" method="POST" action="/openhouse/project/public/auth/login">
-<input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <input type="hidden" name="_token" value="{{ csrf_token() }}">
+          <div class="form-group">
+            <div class="col-md-12">
+              <input type="number" class="form-control" name="student_number" value="{{ old('student_number') }}" placeholder="Student Number">
+            </div>
+          </div>
 
-<div class="form-group">
-<label class="col-md-4 control-label">Student Number</label>
-<div class="col-md-6">
-<input type="number" class="form-control" name="student_number" value="{{ old('student_number') }}">
-</div>
-</div>
+          <div class="form-group">
+            <div class="col-md-12">
+              <input type="password" class="form-control" name="password" placeholder="Password">
+            </div>
+          </div>
 
-<div class="form-group">
-<label class="col-md-4 control-label">Password</label>
-<div class="col-md-6">
-<input type="password" class="form-control" name="password">
-</div>
-</div>
+          <div class="form-group">
+            <div class="col-md-12">
+              <div>
+                <button type="submit" id="log" class="btn btn-primary">Login</button>
+              </div>
+              <div>
+                <button id="reg" class="btn btn-primary">Register</button>
+              </div>
+              <div class="checkbox">
+                <label>
+                  <input type="checkbox" name="remember"> Remember Me
+                </label>
+              </div>
+            </div>
+          </div>
 
-<div class="form-group">
-<div class="col-md-6 col-md-offset-4">
-<div class="checkbox">
-<label>
-<input type="checkbox" name="remember"> Remember Me
-</label>
-</div>
-</div>
-</div>
-
-<div class="form-group">
-  <div class="col-md-6 col-md-offset-4">
-  <button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-    Login
-  </button>
-
-<a href="/password/email">Forgot Your Password?</a>
+          <div class="form-group">
+            <div class="col-md-12">
+              <a href="/password/email">Forgot Your Password?</a>
+            </div>
+          </div>
+        </form>
       </div>
     </div></center>
     <img id="btm" src="http://i.imgur.com/pMAtGEl.png"/>
