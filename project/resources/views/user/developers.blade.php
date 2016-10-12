16440e87f60f8759c8ece73b42fa6c4a7980a3b6
carloerbv <cbvalleramos@up.edu.ph>
2016-10-12 12:44:48 +0800

    added images pero kulang pa

--- project/resources/views/user/developers.blade.php
+++ project/resources/views/user/developers.blade.php
@@ -5,7 +5,7 @@
        	<meta charset="utf-8">
   		  <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/developers.css" type="text/css">
+        <link rel="stylesheet" href="/openhouse/project/public/css/developers.css" type="text/css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
      	
@@ -37,7 +37,7 @@
             <div>
 
               <div class="col-md-5">
-                <img src="http://i.imgur.com/jyOQ6He.jpg" width="200" class="img-circle" height="200">
+                <img src="http://i.imgur.com/ImJxxzf.jpg" width="200" class="img-circle" height="200">
               </div>
 
               <div class="col-md-1"></div>
@@ -50,7 +50,7 @@
                   <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
                   <img src="http://i.imgur.com/MUVn9O2.gif" width="50" height="50">
                 </p>
-                <p>Charlotte Sacmar</p>
+                <p>James Babalcon</p>
                 <p>Graphics</p>
 
               </div>
@@ -74,7 +74,30 @@
                   <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
                   <img src="http://i.imgur.com/Ve1VL4v.gif" width="50" height="50">
                 </p>
-                <p>James Kuizon</p>
+                <p>Jay De Jesus</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/1pbtTof.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/hv2GaTP.gif" width="50" height="50">
+                </p>
+                <p>Joshua Dorado</p>
                 <p>Graphics</p>
 
               </div>
@@ -95,9 +118,171 @@
                   <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
                   <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
                   <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
-                  <img src="http://i.imgur.com/hv2GaTP.gif" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
                 </p>
-                <p>Charlotte Sacmar</p>
+                <p>Maxine Hilvano</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/RS2A2yO.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>James Kuizon</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/RS2A2yO.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Jeff Lucanas</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/tTrvdwY.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Francis Molina</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/bHlKMUX.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Bart Monton</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/bHlKMUX.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Zyra Penaranda</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/UaIQF4z.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Khalile Pujante</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/UaIQF4z.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Albuen Rosello</p>
                 <p>Graphics</p>
 
               </div>
@@ -121,6 +306,52 @@
                   <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
                 </p>
                 <p>Charlotte Sacmar</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/UaIQF4z.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Val Tingson</p>
+                <p>Graphics</p>
+
+              </div>
+            </div>
+          </div>
+
+          <div class="pic col-md-12">
+            <div>
+              <div class="col-md-5">
+                <img src="http://i.imgur.com/pEbA65z.jpg" width="200" class="img-circle" height="200">
+              </div>
+
+              <div class="col-md-1"></div>
+
+              <div class="col-md-6 gob">
+                
+                <p class="pad">
+                  <img src="http://i.imgur.com/eZ77z46.png" width="50" height="50">
+                  <img src="http://i.imgur.com/BzXX5xf.png" width="50" height="50">
+                  <img src="http://i.imgur.com/cW6nHoy.png" width="50" height="50">
+                  <img src="http://i.imgur.com/0QCguSy.gif" width="50" height="50">
+                </p>
+                <p>Carlo Valleramos</p>
                 <p>Graphics</p>
 
               </div>
