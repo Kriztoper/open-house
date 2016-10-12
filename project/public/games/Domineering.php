2d914220502915b294173abe7c24dc4dc916c83d
TOSHIBA-PC\Maria Rebecca Dorado <jrenzo.d@gmail.com>
2016-10-12 14:48:02 +0800

    Added Domineering Game

---
+++ project/public/games/Domineering.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+    <div style="width=2000px;margin:0 auto;">
+    <applet code=main.Main.class archive="Domineering.jar" width = "2000" height="1500">
+    </applet>
+    </div> 
+</body>    
+</html>
