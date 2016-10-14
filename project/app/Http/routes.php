43dac59189b3b31df294282504ed73185fc2265a
Pavilion\HP <jfylucanas@gmail.com>
2016-10-14 18:08:31 +0800

    with routes

--- project/app/Http/routes.php
+++ project/app/Http/routes.php
@@ -11,12 +11,46 @@
 |
 */
 
+/**
+*   Admin for adding videos and genre to database
+*   This will be removed after inserting everything to the database.
+*
+*/
+Route::get('/admin',function(){
+   return view('admin.adminPage'); 
+});
+Route::get('/adminSeries',function(){
+    return view('admin.addSeries');
+});
+Route::get('/adminGenre',function(){
+   return view('admin.addGenre'); 
+});
+Route::get('/adminGame',function(){
+	return view('admin.addGame');
+});
+Route::get('/adminToken',function(){
+	return view('admin.addToken');
+});
+
+//Route::get('/admin', 'AdminController@show_dashboard');
+Route::get('/adminSortGenre','AdminController@getGenre');
+Route::get('/adminGameGenre','AdminController@getGameGenre');
+Route::post('/adminsSeries','AdminController@saveSeries');
+Route::post('/adminsGenre','AdminController@saveGenre');
+Route::post('/adminSortsGenre','AdminController@sortGenre');
+
+/**
+*   Routes for list of videos and watching videos from database
+*
+*/
+Route::get('/series_list','VideosController@listSeries');
+Route::get('/list_video/{id}','VideosController@listVideos');
+Route::get('/watch_video/{id}','VideosController@watchVideos');
 /*
 *	Author: Khalile Pujante
 *	Working login and registration routes
 *	Stable
 */ 
-
 // Default landing page
 Route::get('/', 'Auth\AuthController@getLogin');
 
@@ -34,7 +68,7 @@ Route::post('/register', 'Auth\AuthController@postRegister');
 Route::controllers([
    'password' => 'Auth\PasswordController',
 ]);
-
+Route::get('/videos','VideosController@listSeries');
 Route::group(['middleware' => 'auth'], function () {
     // place your route definitions here
     Route::get('/dashboard'   , 'UserController@show_dashboard');
@@ -43,10 +77,6 @@ Route::group(['middleware' => 'auth'], function () {
 	Route::get('/developers'  , 'UserController@show_developers');
 	Route::get('/game'        , 'UserController@show_games');
 	Route::post('/tokens'     , 'UserController@add_token');
-	Route::get('/videos'      , 'UserController@show_videos');
+	//Route::get('/videos'      , 'UserController@show_videos');
 });
-
-
-//Admin
-Route::get('/admin', 'AdminController@show_dashboard');
 ?>
