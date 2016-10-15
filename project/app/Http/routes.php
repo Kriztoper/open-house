6004b06bbb2cbc5199d779aef3b28dd8a8a94996
CLIENT\user <kkpujante@gmail.com>
2016-10-15 19:36:20 +0800

    modularized routing and flow

--- project/app/Http/routes.php
+++ project/app/Http/routes.php
@@ -11,130 +11,59 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/profile', function () {
-    return view('profile');
-});
-
-Route::get('/hall_of_fame', function () {
-    return view('hall_of_fame');
-});
-
-
-Route::get('/game','GamesController@listGames');
-/**
-* Admin Routes
-*/
-Route::get('/admin',function(){
-   return view('admin.adminPage'); 
-});
-Route::get('/adminSeries',function(){
-    return view('admin.addSeries');
-});
-Route::get('/adminGenre',function(){
-   return view('admin.addGenre'); 
-});
-Route::get('/adminGame',function(){
-  return view('admin.addGames');
-});
-Route::get('/adminToken',function(){
-  return view('admin.addToken');
-});
-
-//Route::get('/admin', 'AdminController@show_dashboard');
-Route::get('/adminSortGenre','AdminController@getGenre');
-Route::get('/adminGameGenre','AdminController@getGameGenre');
-Route::post('/adminGameGenre','AdminController@sortGameGenre');
-Route::post('/adminGame','AdminController@addGame');
-Route::post('/adminsSeries','AdminController@saveSeries');
-Route::post('/adminsGenre','AdminController@saveGenre');
-Route::post('/adminSortsGenre','AdminController@sortGenre');
-Route::post('/addToken','AdminController@generateToken');
-Route::get('/playGame/{id}','GamesController@playGame');
-/**
-*   Routes for list of videos and watching videos from database
-*
-*/
-Route::get('/series_list','VideosController@listSeries');
-
-/* Route::get('/list_video/{id}','VideosController@listVideos');*/
-Route::get('/list_vid',function(){
-   return view('list_vid'); 
-});
-Route::get('/watch_video/{id}','VideosController@watchVideos');
-Route::get('/videos','VideosController@listSeries');
-
-Route::get('/list_video/{id}','VideosController@listVideos');
-
-Route::get('/watch_video/{id}','VideosController@watchVideos');
-Route::get('/videos','VideosController@listSeries');
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
-
-Route::get('/profile', function () {
-    return view('profile');
-});
-
-Route::get('/hall_of_fame', function () {
-    return view('hall_of_fame');
-});
-
-
-/*
-Route::get('/game', function () {
-    return view('game');
-});
-*/
-
-/*
-*	Author: Khalile Pujante
-*	Working login and registration routes
-*	Stable
-*/ 
-
-
-//Time_Usage in progress
-Route::get('/times','UserController@show_Time');
-
-// Default landing page
-Route::get('/', 'Auth\AuthController@getLogin');
-
-// Authentication routes...
-Route::get('/', 'Auth\AuthController@getLogin');
-Route::get('/login', 'Auth\AuthController@getLogin');
-Route::post('/login', 'Auth\AuthController@postLogin');
-Route::get('/logout', 'Auth\AuthController@getLogout');
-
-// Registration routes...
-Route::get('/register', 'Auth\AuthController@getRegister');
-Route::post('/register', 'Auth\AuthController@postRegister');
-
 // Forget Password routes
 Route::controllers([
    'password' => 'Auth\PasswordController',
 ]);
 
-// Routes for user dashboard
+// Authentication routes...
+Route::get('/'          , 'Auth\AuthController@getLogin');
+Route::get('/login'     , 'Auth\AuthController@getLogin');
+Route::post('/login'    , 'Auth\AuthController@postLogin');
+Route::get('/logout'    , 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('/register'  , 'Auth\AuthController@getRegister');
+Route::post('/register' , 'Auth\AuthController@postRegister');
+
+// Routes for normal user
 Route::get('/dashboard'   , 'UserController@show_dashboard');
 Route::get('/hall_of_fame', 'UserController@show_hall_of_fame');
 Route::get('/profile'     , 'UserController@show_profile');
-/*Route::get('/game'		  , 'UserController@show_games');*/
 Route::post('/tokens'	  , 'UserController@add_token');
+Route::get('/game'        , 'GamesController@listGames');
 
+// Admin routes
+Route::get('/admin'           , 'AdminController@show_dashboard');
+Route::get('/adminSeries'     , 'AdminController@show_add_series');
+Route::get('/adminGenre'      , 'AdminController@show_add_genres');
+Route::get('/adminGame'       , 'AdminController@show_add_games');
+Route::get('/adminToken'      , 'AdminController@show_add_tokens');
+Route::get('/adminSortGenre'  , 'AdminController@getGenre');
+Route::get('/adminGameGenre'  , 'AdminController@getGameGenre');
+Route::post('/adminGameGenre' , 'AdminController@sortGameGenre');
+Route::post('/adminGame'      , 'AdminController@addGame');
+Route::post('/adminsSeries'   , 'AdminController@saveSeries');
+Route::post('/adminsGenre'    , 'AdminController@saveGenre');
+Route::post('/adminSortsGenre', 'AdminController@sortGenre');
+Route::post('/addToken'       , 'AdminController@generateToken');
+Route::get('/playGame/{id}'   , 'GamesController@playGame');
 
+// video functionality
+Route::get('/series_list'     , 'VideosController@listSeries');
+Route::get('/list_vid'        , 'VideosController@show_list_vid');
+Route::get('/watch_video/{id}', 'VideosController@watchVideos');
+Route::get('/videos'          , 'VideosController@listSeries');
+Route::get('/list_video/{id}' , 'VideosController@listVideos');
+Route::get('/watch_video/{id}', 'VideosController@watchVideos');
+Route::get('/videos'          , 'VideosController@listSeries');
+
+//Time_Usage in progress
+Route::get('/times'           , 'UserController@show_Time');
 
 /*the next two lines are temporary, used for testing*/
 Route::get('/buyVid/{videoID}', 'TokenController@buy_video');
 Route::get('/buyGame/{gameID}', 'TokenController@buy_game');
 
-
-//	Admin
-Route::get('/admin', 'AdminController@show_dashboard');
-
 ?>
+
