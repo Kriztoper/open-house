39c8fcdffb80cdfb9451786f1af494e15a142e9b
Kriztoper Urmeneta <kriztoperurmeneta@gmail.com>
2017-10-21 17:32:43 +0800

    commit before merge to prev deleted changes

---
+++ project/app/comment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Comment extends Model
+{
+    protected $fillable = ['content', 'forum_id'];
+    public $timestamps = false;
+}
---
+++ project/app/forum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    protected $fillable = ['title'];
+    public $timestamps = false;
+}
