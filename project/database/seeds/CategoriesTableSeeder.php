859f1512c13e5eda08ba48b363abc9482bef4ab2
Kriztoper Urmeneta <kriztoperurmeneta@gmail.com>
2018-08-14 14:15:40 +0800

    Moved old migration files to old-migrations folder

--- project/database/seeds/CategoriesTableSeeder.php
+++ project/database/seeds/CategoriesTableSeeder.php
@@ -16,15 +16,6 @@ class CategoriesTableSeeder extends Seeder
 
         \DB::table('categories')->delete();
         
-        \DB::table('categories')->insert(array (
-            0 => 
-            array (
-                'id' => 1,
-                'category' => 'Entertainment',
-                'created_at' => '2017-12-20 03:59:46',
-                'updated_at' => '2017-12-20 03:59:46',
-            ),
-        ));
         
         
     }
